6120b636b9169964ce2c71d86e633c8cde2913e6
nadirbashir <1.nadirbashir@gmail.com>
2021-05-21 17:41:55 +0500

    eight tasks complete

---
+++ app.js
@@ -0,0 +1,138 @@
+// /******Quest. 1 *********/
+
+// function currentDate(){
+//     var d = new Date();
+//     document.write(d);
+// }
+// currentDate();
+
+// //b)
+// var d;
+// function currentDate(){
+//     d = new Date();
+//     return d;
+// }
+// document.write(currentDate())
+
+// /******Quest. 2 *********/
+// var firstName , lastName;
+// function greet(firstName,lastName){
+//         return "Welcome! " + firstName + " " + lastName;
+// }
+// document.write(greet("Nadir", "Bashir"));
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your first name:");
+// function greet(){
+//     document.write("Welcome! " + firstName + " " + lastName);
+// }
+// greet();
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your first name:");
+// function greet(){
+//     return "Welcome! " + firstName + " " + lastName;
+// }
+// document.write(greet());
+
+// function greet(){
+//     var firstName = prompt("Enter your first name:");
+//     var lastName = prompt("Enter your first name:");
+//     return "Welcome! " + firstName + " " + lastName;
+// }
+// document.write(greet());
+
+// /******Quest. 3 *********/
+// function sum(){
+//     var num1 = +prompt("Enter first Number");
+//     var num2 = +prompt("Enter second Number");
+//     return num1 + num2;
+// }
+// alert("Sum = " + sum());
+
+// /******Quest. 4 *********/
+
+// var num1, num2, op;
+// function calculate(num1, num2, op){
+//     if(op === "+"){
+//         return num1 + num2;
+//     }
+//     else if(op === "-"){
+//         return num1 - num2;
+//     }
+//     else if(op === "*"){
+//         return num1 * num2;
+//     }
+//     else if(op === "/"){
+//         return num1 / num2;
+//     }
+//     else if(op === "%"){
+//         return num1 % num2;
+//     }
+//     else if(op === "**"){
+//         return num1 ** num2;
+//     }
+//     else{
+//         return "invalid operator";
+//     }
+// }
+// document.write("<br>" + calculate(8,4,"+"));
+// document.write("<br>" + calculate(8,4,"-"));
+// document.write("<br>" + calculate(8,4,"*"));
+// document.write("<br>" + calculate(8,4,"/"));
+// document.write("<br>" + calculate(8,4,"%"));
+// document.write("<br>" + calculate(8,4,"**"));
+// document.write("<br>" + calculate(8,4,"&"));
+
+// /******Quest. 6 *********/
+// function counting(start,endVal){
+    
+//     for(var i = start; i <= endVal; i++){
+//         document.write(i + "<br>");
+//     }
+// }
+// counting(+prompt("Enter starting value:"), +prompt("Enter End value:"));
+
+// /******Quest. 7 *********/
+// function fact(fact){
+//     var num = fact;
+//     for(var i = 1; i < fact ; i++){
+//         num *= i;
+//     }
+//     return num;
+// }
+// document.write(fact(10));
+
+// // b)
+// function fact(fact){
+//     var factorial = fact;
+//   for(var i = fact - 1; i > 0 ; i--){
+//     factorial *= i;
+//   }
+//   return factorial;
+// }
+// document.write(fact(10));
+
+// /******Quest. 8 *********/
+
+// function CalculateHypotenuse(){
+//     var base = +prompt("Enter Base");
+//     var perp = +prompt("Enter Perpendicular");
+//     var hyp = (base ** 2) + (perp ** 2);
+//     function calculateSquare(){
+//         return Math.sqrt(hyp);
+//     }
+//     return "Hypotenous = " + calculateSquare();
+// }
+// document.write(CalculateHypotenuse());
+
+
+// /******Quest. 9 *********/
+
+
+
+// /******Quest. 1 *********/
+// /******Quest. 1 *********/
+// /******Quest. 1 *********/
+// /******Quest. 1 *********/
+// /******Quest. 1 *********/
